fd8018b0ab5c0786d8a56bd804e29d0d9ef89241
km1031kim <80741423+km1031kim@users.noreply.github.com>
2025-01-14 10:40:03 +0900

    Initial commit

---
+++ src/main/java/io/goorm/mini/MiniApplication.java
@@ -0,0 +1,14 @@
+package io.goorm.mini;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniApplication.class, args);
+	}
+
+}
